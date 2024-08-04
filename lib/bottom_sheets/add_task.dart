016911247e5042ad040e8b08bc92f22b9a20bdaa
AlaaRaslan <alaaraslan152@gmail.com>
2024-08-04 21:07:30 +0300

    sess 1

--- lib/bottom_sheets/add_task.dart
+++ lib/bottom_sheets/add_task.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/app_colors.dart';
 
-class AddTask extends StatelessWidget {
-  const AddTask({super.key});
+class AddTask extends StatefulWidget {
+  AddTask({super.key});
 
+  @override
+  State<AddTask> createState() => _AddTaskState();
+}
+
+class _AddTaskState extends State<AddTask> {
+  DateTime selectedDate = DateTime.now();
+
+  
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -37,12 +45,9 @@ class AddTask extends StatelessWidget {
             SizedBox(height: 15,),
             InkWell(
               onTap: () {
-                showDatePicker(
-                    context: context,
-                    firstDate: firstDate,
-                    lastDate: lastDate)
+                selectDateFun();
               },
-              child: Text("7/7/2002",textAlign: TextAlign.center,
+              child: Text(selectedDate.toString().substring(0,10),textAlign: TextAlign.center,
                   style:  TextStyle(fontSize: 15,color: AppColors.primary )),
             ),
             SizedBox(height: 15,),
@@ -55,5 +60,24 @@ class AddTask extends StatelessWidget {
           ],
       ),
     ) ;
+  }
+
+  selectDateFun()async{
+    DateTime? chodenDate = await showDatePicker(
+        context: context,
+        builder: (context, child) => Theme(
+          data: Theme.of(context).copyWith(colorScheme: ColorScheme.light(
+              primary: AppColors.primary,
+          )),
+          child:child! ,
+        ),
+        initialDate: selectedDate,
+        firstDate: DateTime.now(),
+        lastDate: DateTime.now().add(Duration(days: 365)));
+    if(chodenDate!=null){
+      selectedDate=chodenDate;
+      setState(() {});
+
+    }
   }
 }
