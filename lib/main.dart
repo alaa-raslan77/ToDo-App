4df5def13850d774afbc954b00b1c60fd96148d8
AlaaRaslan <alaaraslan152@gmail.com>
2024-08-10 04:59:53 +0300

    firebase start

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/providers/my_provider.dart';
+import 'firebase_options.dart';
 import 'my_app.dart';
 
 void main()async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await EasyLocalization.ensureInitialized();
 
   runApp(ChangeNotifierProvider(
